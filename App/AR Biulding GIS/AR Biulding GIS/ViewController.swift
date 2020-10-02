81222696417c41b83126cf2cbc821ca97786dfdb
brazilec22 <stratienko.artem@gmail.com>
2020-10-02 20:52:45 +0300

    auto commit rep - dsh - AR Building GIS

---
+++ App/AR Biulding GIS/AR Biulding GIS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AR Biulding GIS
+//
+//  Created by Artem Stratienko on 02.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
